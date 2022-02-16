05188658d0b0e4ba7780234d8db0ab4c260ae392
Tim Grannen <grannendev@gmail.com>
2022-02-16 00:42:40 -0500

    Add Fight Status

--- wordle/dotnet/BotFights.Wordle/API/WordleFight.cs
+++ wordle/dotnet/BotFights.Wordle/API/WordleFight.cs
@@ -9,8 +9,8 @@ public class WordleFight
     [JsonPropertyName("guess_cnt")] public int? GuessCount { get; set; }
     [JsonPropertyName("round_num")] public int? RoundNumber { get; set; }
     [JsonPropertyName("wordlist")] public string? Wordlist { get; set; }
+    [JsonPropertyName("status")] public string? Status { get; set; }
     [JsonPropertyName("feedback")] 
     [JsonConverter(typeof(PropertyArrayOfStringsConverter))]
     public List<string>? Feedback { get; set; }
-    
 }
